dd34679c4cbd64b80897c66165f0173473dcc87e
leo1mml <mml_leo@hotmail.com>
2018-02-17 18:50:41 -0200

    trying to fix xml reqs againt

--- server/server.js
+++ server/server.js
@@ -14,12 +14,24 @@ let port = process.env.PORT
 let app = express()
  
 app.use(bodyParser.json())
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "http://localhost:3000");
-  res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,app-pass,x-auth');
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
   next();
-})
+});
 
 app.use('/users', users)
 app.use('/teachers', teachers)
