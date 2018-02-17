54b9b832a78249a425aad7009ed9397aea2f8b09
leo1mml <mml_leo@hotmail.com>
2018-02-17 20:38:08 -0200

    trying to fix path to access control origin

--- server/middleware/app-pass.js
+++ server/middleware/app-pass.js
@@ -1,7 +1,7 @@
 var appPass = (req, res, next) => {
     const appToken = req.header('app-pass')
     if(appToken === process.env.APP_SECRET_TOKEN){
-        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000/')
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
         next()
     }else {
         res.status(401).send()
