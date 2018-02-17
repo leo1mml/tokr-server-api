9bac8699b67b9c2b0f60af90c38d7a2b3e16e54c
leo1mml <mml_leo@hotmail.com>
2018-02-17 19:51:39 -0200

    added cors from npm

--- server/server.js
+++ server/server.js
@@ -9,28 +9,12 @@ const users = require('./routers/users')
 const teachers = require('./routers/teachers')
 const students = require('./routers/students')
 const classes = require('./routers/classes')
+const cors = require('cors')
 
 let port = process.env.PORT
 let app = express()
 
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,app-pass,x-auth');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
+app.use(cors());
 
 app.use(bodyParser.json())
 
