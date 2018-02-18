011b4b04d4b8d0296118b2209b4e973b878f68a2
leo1mml <mml_leo@hotmail.com>
2018-02-18 19:03:57 -0300

    wtf error

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
   if(req.method === 'OPTIONS'){
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
     return res.status(200).json({})
-  }
+  } 
   next()
 });
 
